dd905cace454d2c4133db2fccf7fc2a6acef8e87
anujverma132 <anuj.verma1320@gmail.com>
2021-08-26 17:56:37 +0530

    Fourth commit

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -32,12 +32,11 @@ export default function Textform(props) {
       <div className="container my-3">
         <h1 className='my-3' style={{color : props.mode==='dark'?'white':'#042743'}}>Hello React! This is My First App</h1>
         {/* <p style={{color : props.mode==='dark'?'white':'#042743'}}>This is a Text Util App</p> */}
-          <textarea style={{backgroundColor: props.mode==='dark'?'grey':'white',color : props.mode==='dark'?'white':'black' , width:'98%'}} rows="8" cols="150" value={text} onChange={textData} />
+          <textarea style={{backgroundColor: props.mode==='dark'?'grey':'white',color : props.mode==='dark'?'white':'black' , width:'100%'}} rows="8" cols="150" value={text} onChange={textData} />
           <div className="my-2">
-            <button onClick={toUppercase} className="btn btn-primary"> Convert To Upper case </button>
-            <button onClick={toLowercase} className="btn btn-primary mx-2"> Convert To Lower case </button>
+            <button style={{margin:'10px'}} onClick={toUppercase} className="btn btn-primary"> Convert To Upper case </button>
+            <button style={{margin:'10px'}} onClick={toLowercase} className="btn btn-primary mx-2"> Convert To Lower case </button>
           </div>
-
       </div>
       <div className="container my-3">
         <h1 style={{color : props.mode==='dark'?'white':'#042743'}}>Text Analysis is here!</h1>
